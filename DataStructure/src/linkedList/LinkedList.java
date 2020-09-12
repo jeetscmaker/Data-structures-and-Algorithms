bb0ecca83f79b191ba4bffe95512229a2d9a23bd
Jitendra <jeetscmaker@gmail.com>
2020-09-12 13:21:26 +0530

    ListNode and a Single Linked List data structure created.

---
+++ DataStructure/src/linkedList/LinkedList.java
@@ -0,0 +1,60 @@
+package linkedList;
+
+/**
+ * A single LinkedList data structure.
+ */
+public class LinkedList {
+	private int length;
+	private ListNode head;
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	/* Return the first node in the list */
+	public synchronized ListNode getHead() {
+		return head;
+	}
+
+	public void setHead(ListNode head) {
+		this.head = head;
+	}
+
+	public LinkedList(int length, ListNode head) {
+		this.length = length;
+		this.head = head;
+	}
+	
+	public LinkedList() {
+		length = 0;
+		head = null;
+	}
+	
+	public LinkedList(ListNode head) {
+		this.length = 0;
+		this.head = head;
+	}
+	
+	/* listLength function takes head node as input and returns the total number of nodes in the 
+	 * linkedList 
+	 * */
+	public int listLength(ListNode head) {
+		int length = 0;
+		ListNode currentNode = head;
+		while(currentNode != null) {
+			length++;
+			currentNode = currentNode.getNext();
+		}
+		return length;
+	}
+	/* Insert a node at the beginning of the list */
+	public synchronized void insertAtBegin(ListNode node) {
+		node.setNext(head);
+		head = node;
+		length++;
+	}
+	
+}
