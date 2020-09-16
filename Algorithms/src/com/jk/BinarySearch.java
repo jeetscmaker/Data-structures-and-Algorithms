cee8c88c748bcff221947c5d0f9eb27c97a917ba
Jitendra <jeetscmaker@gmail.com>
2020-09-16 23:39:18 +0530

    Binary search using recursion added, test cases also added for that.

--- Algorithms/src/com/jk/BinarySearch.java
+++ Algorithms/src/com/jk/BinarySearch.java
@@ -1,6 +1,7 @@
 package com.jk;
 
 public class BinarySearch {
+	/* Binary search using Iterative approach */
 	public static int binarySearch(int[] A, int data) {
 		int low = 0, high = A.length - 1, mid;
 		while (low <= high) {
@@ -16,6 +17,19 @@ public class BinarySearch {
 		return -1;
 	}
 
+	/* Binary search using recursion */
+	public static int binarySearchRecursion(int[] A, int data, int low, int high) {
+		int mid = low + (high - low) / 2;
+		if (A[mid] == data)
+			return mid;
+		else if (A[mid] < data)
+			return binarySearchRecursion(A, data, mid + 1, high);
+		else if (A[mid] > data)
+			return binarySearchRecursion(A, data, low, mid - 1);
+		else
+			return -1;
+	}
+
 	public static void main(String[] args) {
 		int[] arr = { 2, 4, 5, 12, 44, 67, 71 };
 		int a = 65;
@@ -24,5 +38,11 @@ public class BinarySearch {
 		System.out.println(binarySearch(arr, a)); // should print 5
 		a = 12;
 		System.out.println(binarySearch(arr, a)); // should print 3
+		a = 67;
+		System.out.println(binarySearchRecursion(arr, a, 0, 6)); // should print 5
+		a = 12;
+		System.out.println(binarySearchRecursion(arr, a, 0, 6)); // should print 3
+		a = 44;
+		System.out.println(binarySearchRecursion(arr, a, 0, 6)); // should print 4
 	}
 }
