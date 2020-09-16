1f65251df4457cf74ba350df053a752c6b37b022
Jitendra <jeetscmaker@gmail.com>
2020-09-16 23:15:40 +0530

    Binary search algorithm with test case inside main method.

---
+++ Algorithms/src/com/jk/BinarySearch.java
@@ -0,0 +1,24 @@
+package com.jk;
+
+public class BinarySearch {
+	public static int binarySearch(int[] A, int data) {
+		int low = 0, high = A.length - 1, mid;
+		while (low <= high) {
+			mid = low + (high - low) / 2;
+			if (data == A[mid]) {
+				return mid;
+			} else if (data < A[mid]) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 2, 4, 5, 12, 44, 67, 71 };
+		int a = 65;
+		System.out.println(binarySearch(arr, a));
+	}
+}
