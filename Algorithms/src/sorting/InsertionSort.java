18b67aa029154a66506e3a0fec19e22ac201fd93
Jitendra <jeetscmaker@gmail.com>
2020-09-17 19:46:15 +0530

    Insertion sort algorithm with test case.

---
+++ Algorithms/src/sorting/InsertionSort.java
@@ -0,0 +1,37 @@
+package sorting;
+
+public class InsertionSort {
+	
+	/* This method takes an input array and sorts the elements and returns the sorted array back. */
+	public static int[] insertionSort(int[] arr) {
+		int i, j, temp, key;
+		for(i=1; i<arr.length; i++) {
+			j = i-1;
+			key = arr[i];
+			while(j>=0 && key < arr[j]) {
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+				j--;
+			}
+		}
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {5, 8, 1, 3, 9, 6};
+		// before sorting array
+		printArray(array);
+		array = insertionSort(array);
+		// after sorting array
+		printArray(array);
+	}
+	
+	/* printing the elements of the given array */
+	public static void printArray(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+}
