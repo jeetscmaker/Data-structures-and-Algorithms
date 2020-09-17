9a71c3a6326c8c389766c9b11166996b5f124744
Jitendra <jeetscmaker@gmail.com>
2020-09-17 19:53:23 +0530

    Utility class added for common methods.

--- Algorithms/src/sorting/InsertionSort.java
+++ Algorithms/src/sorting/InsertionSort.java
@@ -21,17 +21,9 @@ public class InsertionSort {
 	public static void main(String[] args) {
 		int[] array = {5, 8, 1, 3, 9, 6};
 		// before sorting array
-		printArray(array);
+		Utility.printArray(array);
 		array = insertionSort(array);
 		// after sorting array
-		printArray(array);
-	}
-	
-	/* printing the elements of the given array */
-	public static void printArray(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println();
+		Utility.printArray(array);
 	}
 }
---
+++ Algorithms/src/sorting/Utility.java
@@ -0,0 +1,11 @@
+package sorting;
+
+public class Utility {
+	/* printing the elements of the given array */
+	public static void printArray(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+}
