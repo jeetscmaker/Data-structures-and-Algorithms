5303dae3a0fc7eb57401a4a8c8808d1e3067e1e9
Jitendra <jeetscmaker@gmail.com>
2020-09-30 23:54:58 +0530

    pussh() and pop() operations added.

--- DataStructure/src/stack/Stack.java
+++ DataStructure/src/stack/Stack.java
@@ -4,5 +4,47 @@ package stack;
  * Simple array based implementation of Stack data structure.
  */
 public class Stack {
+	// length of the array used to implement stack.
+	protected int capacity;
+	// Default array capacity.
+	public static final int CAPACITY = 10;
+	// Array used to implement stack.
+	protected int[] stackRep;
+	// index of the top element in the stack;
+	protected int top = -1;
+
+	// initialize the stak with default capacity
+	public Stack() {
+		this(CAPACITY);
+	}
+
+	public Stack(int cap) {
+		this.capacity = cap;
+		stackRep = new int[capacity];
+	}
+
+	public int size() {
+		return top + 1;
+	}
+
+	public boolean isEmpty() {
+		return top < 0;
+	}
+
+	// insert an element.
+	public void push(int data) throws Exception {
+		if (size() == capacity)
+			throw new Exception("Stack is full");
+		stackRep[++top] = data;
+	}
+
+	// Removes the element from the top of the stack.
+	public int pop() throws Exception {
+		if (isEmpty())
+			throw new Exception("Stack is empty.");
+		int data = stackRep[top];
+		stackRep[top--] = Integer.MIN_VALUE;
+		return data;
+	}
 
 }
