b58518a8ac3a391c92d5dcaf65b00c5f249afbc2
Jitendra <jeetscmaker@gmail.com>
2020-10-06 23:40:58 +0530

    Queue data structure with test cases added.

---
+++ DataStructure/src/queue/TestQueue.java
@@ -0,0 +1,21 @@
+package queue;
+
+public class TestQueue {
+    public static void main(String[] args) {
+        Queue queue = new Queue(10);
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+
+        System.out.println(queue.dequeue()
+                + " dequeued from queue\n");
+
+        System.out.println("Front item is "
+                + queue.front());
+
+        System.out.println("Rear item is "
+                + queue.rear());
+    }
+}
