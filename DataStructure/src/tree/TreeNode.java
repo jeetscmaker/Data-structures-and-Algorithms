8903e4774bf1b729c637685ce2da46001eeb5b37
Jitendra Pandey <jeetscmaker@gmail.com>
2020-10-13 19:28:34 +0530

    Create TreeNode.java
    TreeNode.java file added for n-ary tree representation.

---
+++ DataStructure/src/tree/TreeNode.java
@@ -0,0 +1,47 @@
+package org.jk;
+/**
+* Fundamental data structure for n-ary tree.
+* 
+* @author Jitendra
+*/
+public class TreeNode{
+  private int data;
+  private TreeNode firstChild;
+  private TreeNode nextSibling;
+  
+  public TreeNode(int data){
+    this.data = data;
+    firstChild = null;
+    nextSibling = null;
+  }
+  
+  public TreeNode(){
+    data = 0;
+    firstChild = null;
+    nextSibling = null;
+  }
+  
+  public int getData(){
+    return data;
+  }
+  
+  public void setData(int data){
+    this.data = data;
+  }
+  
+  public TreeNode getFirstChild(){
+    return firstChild;
+  }
+  
+  public void setFirstChild(TreeNode node){
+    this.firstChild = node;
+  }
+  
+  public TreeNode getNextSibling(){
+    return nextSibling;
+  }
+  
+  public void setNextSibling(TreeNode node){
+    this.nextSibling = node;
+  }
+}
