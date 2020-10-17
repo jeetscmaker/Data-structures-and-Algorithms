a8c57bb9509332aae90dff12daf00c8b184e1e44
Jitendra Pandey <jeetscmaker@gmail.com>
2020-10-17 19:40:07 +0530

    mirror of a tree.

--- Algorithms/src/problems/TreePathSum.java
+++ Algorithms/src/problems/TreePathSum.java
@@ -1,9 +1,11 @@
-/**
-Give an algorithm for checking the existence of path win given sum.
+package problems;
 
-For this problem, the strategy is subtract the mode value from The Sum before calling its children recur And chief at the sum is zero ar we run out of TreeNode 
-*/
 public class TreePathSum{
+
+/**
+* Give an algorithm for checking the existence of path win given sum.
+* For this problem, the strategy is subtract the mode value from The Sum before calling its children recur And chief at the sum is zero ar we run out of TreeNode 
+*/
 public boolean hasPathSum(TreeNode root, int sum){
   if (root == null) return false;
   if (root.left == null && root.right == null && root.data == sum)
@@ -11,4 +13,19 @@ public boolean hasPathSum(TreeNode root, int sum){
   else 
     return hasPathSum(root.left, sum – root.data) || hasPathSum(root.right, sum - root.data); 
 }
+/**
+* Give an algorithm for converting a tree to its mirror.
+*/
+public BinaryTreeNode mirror(BinaryTreeNode root) {
+    BinaryTreeNode temp;
+    if(root != null) {
+        mirror(root.left);
+        mirror(root. right);
+        // swapping the elements
+        temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+      } 
+      return root:
+    }
 }
