625c56016a81a0e53f3dac84f7bebddc68934677
Jitendra Pandey <jeetscmaker@gmail.com>
2020-10-18 18:13:30 +0530

    Create Tree problems.java

---
+++ DataStructure/src/tree/Tree problems.java
@@ -0,0 +1,25 @@
+package tree;
+
+public class TreeProblems{
+  /* Give an algorithm for finding the vertical sums in a binary tree. */
+ public static void vSum(HashMap <Integer, Integer> hash, BinaryTreeNode root, int c){
+    if (root.left!=null) 
+        vSum(hash, root.left, c-1);
+    if (root.right!=null)
+        vSum(hash,root.right, c+1);
+    int data=0; 
+    if (hash.containsKey(c))
+        data=hash.get(c); 
+    hash.put(c, root.data+data);
+  }
+
+ public static void verticalSum(BinaryTreeNode root){
+    HashMap <Integer, Integer> hash = new HashMap<Integer, Integer>();
+    vSum(hash, root, 0);
+    System.out.println();
+    for(int k:hash.keySet(){
+        System.out.println("key(pos): "+k+" sum: "+ hash.get(k)+" ");
+      
+     }
+    }
+}
