902263dbb65ebbacfd1e2cea0a5b902b944e6c08
Jitendra <jeetscmaker@gmail.com>
2020-10-22 23:49:15 +0530

    AVL tree node.

---
+++ DataStructure/src/tree/AVLNode.java
@@ -0,0 +1,25 @@
+package tree;
+
+/* Class AVLNode */
+
+public class AVLNode {
+    AVLNode left, right;
+    int data;
+    int height;
+
+    /* Constructor */
+    public AVLNode() {
+        left = null;
+        right = null;
+        data = 0;
+        height = 0;
+    }
+
+    /* Constructor */
+    public AVLNode(int n) {
+        left = null;
+        right = null;
+        data = n;
+        height = 0;
+    }
+}
