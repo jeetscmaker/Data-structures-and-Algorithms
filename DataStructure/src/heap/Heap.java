29295b4d7a5a067af185d0cabb388d4ca5eed119
Jitendra <jeetscmaker@gmail.com>
2020-10-27 23:57:37 +0530

    Heap data structure.

---
+++ DataStructure/src/heap/Heap.java
@@ -0,0 +1,37 @@
+package heap;
+
+/**
+ * @author Jitendra
+ * A heap data structure is a tree with following properties:
+ * i). value of the node must be >= its children( in max_heap)
+ * value of the node must be <= its children( in min_heap)
+ * ii). All the nodes should be at height h or h-1, where h is
+ * the height of the tree.
+ */
+public class Heap {
+    public int[] array;
+    public int count;
+    public int capacity;
+    public int heapType; // max_heap or min_heap
+
+    public Heap(int capacity, int heapType) {
+        this.heapType = heapType;
+        this.capacity = capacity;
+        this.count = 0;
+        this.array = new int[capacity];
+    }
+
+    /* Parent of a node. */
+    public int parent(int i) {
+        if (i <= 0 || i >= this.count)
+            return -1;
+        return (i - 1) / 2;
+    }
+
+    /* Getting the maximum element. */
+    public int getMax(){
+        if (this.count == 0)
+            return -1;
+        return this.array[0];
+    }
+}
