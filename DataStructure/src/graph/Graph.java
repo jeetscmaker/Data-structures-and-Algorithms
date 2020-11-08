be4fa1e66a7cba05d3f4e3b9593cda27b647d2ec
Jitendra <jeetscmaker@gmail.com>
2020-11-08 23:57:27 +0530

    Graph and an auxiliary Vertex class.

---
+++ DataStructure/src/graph/Graph.java
@@ -0,0 +1,45 @@
+package graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Graph {
+    private final int maxVertices = 20;
+    private Vertex[] vertexList;
+    private int[][] adjMatrix;
+    private int vertexCount;
+    private Stack stack; // for DFS
+    private Queue queue; // for BFS
+
+    public Graph(){
+        vertexList = new Vertex[maxVertices];
+        vertexCount = 0;
+        stack = new Stack();
+        queue = new LinkedList();
+        for (int i = 0; i < maxVertices; i++) {
+            for (int j = 0; j < maxVertices; j++) {
+                adjMatrix[i][j] = 0;
+            }
+        }
+    }
+
+    public void dfs(){
+        //TODO
+    }
+
+    public void bfs(){
+        //TODO
+    }
+}
+
+class Vertex {
+    public char label;
+    public boolean visited;
+
+    public Vertex(char l) {
+        label = l;
+        visited = false;
+    }
+}
