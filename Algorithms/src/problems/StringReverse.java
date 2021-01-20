7d1308cb1140662846c94ee2c83e5e95fc310edb
Jitendra <jeetscmaker@gmail.com>
2021-01-20 19:06:30 +0530

    String reverse using logic.

--- Algorithms/src/problems/StringReverse.java
+++ Algorithms/src/problems/StringReverse.java
@@ -3,7 +3,9 @@ package problems;
 public class StringReverse {
     public static void main(String[] args) {
         String input = "abcde";
-        System.out.println("The reversed string is: " + reverse(input));
+        System.out.println("The reversed string using reverse() is: " + reverse(input));
+        System.out.println("The reversed string using reverse2() is: " + reverse2(input));
+        System.out.println("The reversed string using reverse3() is: " + reverse3(input));
     }
 
     // Using recursion.
@@ -17,8 +19,16 @@ public class StringReverse {
     }
 
     // Using StringBuilder.
-    public String reverse2(String input) {
+    public static String reverse2(String input) {
         StringBuilder sb = new StringBuilder(input);
         return sb.reverse().toString();
+    }
+
+    public static String reverse3(String input){
+        String reversed = "";
+        for (int i = input.length()-1; i >=0 ; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+        return reversed;
     }
 }
