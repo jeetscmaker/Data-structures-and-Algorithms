96fd4b6808178da56e2e5e8ef2146f851226b4ef
Jitendra <jeetscmaker@gmail.com>
2021-02-26 23:23:10 +0530

    Maximum sum contiguous subarray.

---
+++ Algorithms/src/problems/MaxContiguousSumSubarray.java
@@ -0,0 +1,30 @@
+package problems;
+
+/**
+ * Find the maximum sum of a contiguous subarray of size k in a given array
+ * Input  : arr[] = {100, 200, 300, 400}, k = 2, Output : 700 {300,400}
+ * Input  : arr[] = {1, 4, 2, 10, 23, 3, 1, 0, 20}, k = 4, Output : 39 {4, 2, 10, 23}
+ * */
+public class MaxContiguousSumSubarray {
+    public static void main(String[] args) {
+        int[] arr = {100, 200, 300, 400}; // k = 2
+        System.out.println("Maximum sum contiguous subArray" + maxSum(arr, 2));
+    }
+    public static int maxSum(int[] array, int k){
+        int maxSum = 0;
+        int n = array.length, sum = 0;
+        int startpointer = 0;
+        for(int i=0; i<k; i++){
+            sum += array[i];
+        }
+        startpointer++;
+        for(int i = k; i<n; i++){
+            if(maxSum < sum)
+                maxSum = sum;
+            sum = sum-array[startpointer] + array[i];
+            startpointer++;
+        }
+        return maxSum;
+    }
+
+}
