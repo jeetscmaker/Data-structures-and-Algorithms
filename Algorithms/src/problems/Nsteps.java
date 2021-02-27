7f7138b1c837d460ace879bf2bfcfe9fe6c482d0
Jitendra <jeetscmaker@gmail.com>
2021-02-27 18:19:01 +0530

    Number of ways calculation for taking the steps using recursion

---
+++ Algorithms/src/problems/Nsteps.java
@@ -0,0 +1,24 @@
+package problems;
+
+/**
+ * There are n-stairs, a person standing at the bottom wants to reach the top. The person can climb either 1 stair or 2 stairs at a time. Count the number of ways,
+ * the person can reach the top
+ * n=1, there is only one way
+ * n=2, There are two ways: (1, 1) and (2)
+ * n = 3, 1,1,1   1,2   2,1
+ * n = 4, there are 5 ways: (1, 1, 1, 1), (1, 1, 2), (2, 1, 1), (1, 2, 1), (2, 2)
+ */
+
+public class Nsteps {
+    public static void main(String[] args) {
+        System.out.println(calNumberOfWays_recursion(5));
+    }
+// Similar to fibonacci series recursion.
+    public static int calNumberOfWays_recursion(int n){
+        if(n ==1)
+            return 1;
+        if(n==2)
+            return 2;
+        else return calNumberOfWays_recursion(n-1) + calNumberOfWays_recursion(n-2);
+    }
+}
