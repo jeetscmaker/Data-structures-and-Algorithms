f4199281188c1e907aae3687bbbdb0a7ce87a9f0
Jitendra <jeetscmaker@gmail.com>
2021-06-04 02:22:25 +0530

    contest: Codeforces Round #377 (Div. 2), problem: (A) Buy a Shovel

--- Algorithms/src/codeforces/Main.java
+++ Algorithms/src/codeforces/Main.java
@@ -3,16 +3,20 @@ package codeforces;
 import java.util.Scanner;
 
 /**
- * Greatest Common Divisor of two numbers.
+ * contest: Codeforces Round #377 (Div. 2), problem: (A) Buy a Shovel.
  */
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a, b;
-        a = sc.nextInt();
-        b = sc.nextInt();
-        System.out.println("GCD is: " + gcd(a, b));
-        System.out.println("LCM is: " + lcm(a, b));
+        int k, r;
+        k = sc.nextInt();
+        r = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            if ((k * i) % 10 == r || (k * i) % 10 == 0) {
+                System.out.println(i);
+                break;
+            }
+        }
         sc.close();
     }
 
