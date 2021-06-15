8bc6464a5775e47fa659a14be68896d6dad1e5c6
Jitendra <jeetscmaker@gmail.com>
2021-06-16 02:12:41 +0530

    Codeforces Round #719 (Div. 3), problem: (A) Do Not Be Distracted!

--- Algorithms/src/codeforces/Main.java
+++ Algorithms/src/codeforces/Main.java
@@ -3,37 +3,32 @@ package codeforces;
 import java.util.Scanner;
 
 /**
- * Find the maximum sum of a contiguous subarray of size k in a given array
- * Input  : arr[] = {100, 200, 300, 400}, k = 2, Output : 700 {300,400}
- * Input  : arr[] = {1, 4, 2, 10, 23, 3, 1, 0, 20}, k = 4, Output : 39 {4, 2, 10, 23}
- * */
+ * Codeforces Round #719 (Div. 3), problem: (A) Do Not Be Distracted!, Accepted
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n, k;
-        n = sc.nextInt();
-        k = sc.nextInt();
-        int a[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-        }
-        int sum = 0, max_sum = 0;
-        // sum of first k elements
-        for (int i = 0; i < k; ++i) {
-            sum += a[i];
-        }
-        int start = 0;
-        for (int i = k; i < n; ++i) {
-            if (sum > max_sum) {
-                max_sum = sum;
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            String s = sc.next();
+            boolean isStringOk = true;
+            OUTER:
+            for (int j = 0; j < n; j++) {
+                char c = s.charAt(j);
+                int k = s.lastIndexOf(c);
+                for (int l = j; l <= k; l++) {
+                    if (s.charAt(l) != c) {
+                        isStringOk = false;
+                        break OUTER;
+                    }
+                }
             }
-            sum = sum - a[start] + a[i];
-            start++;
+            if (isStringOk)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
         }
-        if (sum > max_sum) {
-            max_sum = sum;
-        }
-        System.out.println("Max sum is: " + max_sum);
         sc.close();
     }
 
