f897dc38b3817631e5be99ba75130f3a7cf5205b
Jitendra <jeetscmaker@gmail.com>
2021-06-21 00:14:05 +0530

    //  contest: Codeforces Round #727 (Div. 2), problem: (B) Love Song, Time limit exceeded on test 4

--- Algorithms/src/codeforces/Main.java
+++ Algorithms/src/codeforces/Main.java
@@ -1,70 +1,26 @@
 package codeforces;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
-/**
- * contest: Codeforces Round #656 (Div. 3), problem: (B) Restore the Permutation by Merger.
- */
+import java.util.*;
+//  contest: Codeforces Round #727 (Div. 2), problem: (B) Love Song, Time limit exceeded on test 4
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        for (int i = 0; i < t; i++) {
-            int n = sc.nextInt();
-            ArrayList<Integer> al = new ArrayList<>();
-            for (int j = 0; j < 2 * n; j++) {
-                al.add(sc.nextInt());
+        int n, q;
+        n = sc.nextInt();
+        q = sc.nextInt();
+        String s = sc.next();
+        char ch = 'a';
+        int l, r;
+        for (int i = 0; i < q; i++) {
+            l = sc.nextInt();
+            r = sc.nextInt();
+            int length = 0;
+            String s1 = s.substring(l - 1, r);
+            for (char c : s1.toCharArray()) {
+                length += c - ch + 1;
             }
-            ArrayList<Integer> res = new ArrayList<>();
-            for (int j = 0; j < n; j++) {
-                Integer element = al.get(0);
-                res.add(element);
-                for (int k = 0; k < 2; k++) {
-                    al.remove(element);
-                }
-            }
-            for (int j = 0; j < res.size(); j++) {
-                System.out.print(res.get(j) + " ");
-            }
+            System.out.println(length);
         }
         sc.close();
-    }
-
-    // n is not composite simply says that it must be a prime number
-    private static boolean isPrime(int d) {
-        for (int i = 2; i <= Math.sqrt(d); i++) {
-            if (d % i == 0)
-                return false;
-        }
-        return true;
-    }
-
-    private static boolean is_not_divisible(int k, int l, int m, int n, int d) {
-        if ((d % k != 0) && (d % l != 0) && (d % m != 0) && (d % n != 0))
-            return true;
-        else
-            return false;
-    }
-
-    // finding GCD of two numbers iteratively.
-    public static int gcd(int a, int b) {
-        int big = a > b ? a : b;
-        int small = a < b ? a : b;
-        int remainder = big % small;
-        while (remainder != 0) {
-            big = small;
-            small = remainder;
-            remainder = big % small;
-        }
-        return small;
-    }
-
-    public static int gcd_r(int a, int b) {
-        return (b == 0 ? a : gcd_r(b, a % b));
-    }
-
-    public static int lcm(int a, int b) {
-        return (a * b) / gcd(a, b);
     }
 }
