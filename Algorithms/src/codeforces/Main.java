37726c852c1fb0ebb7ba1d4010a35b72c5e7f22b
Jitendra <jeetscmaker@gmail.com>
2021-06-22 00:20:12 +0530

    contest: April Fools Day Contest 2021, problem: (A) Is it rated - 2

--- Algorithms/src/codeforces/Main.java
+++ Algorithms/src/codeforces/Main.java
@@ -1,25 +1,16 @@
 package codeforces;
 
 import java.util.Scanner;
-// contest: Codeforces Round #107 (Div. 2), problem: (A) Soft Drinking,
+// contest: April Fools Day Contest 2021, problem: (A) Is it rated - 2
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // positive integers n, k, l, c, d, p, nl, np, not exceeding 1000 and no less than 1
-        int n,k,l,c,d,p,nl,np;
-        n = sc.nextInt();
-        k = sc.nextInt();
-        l = sc.nextInt();
-        c = sc.nextInt();
-        d = sc.nextInt();
-        p = sc.nextInt();
-        nl = sc.nextInt();
-        np = sc.nextInt();
-
-        int soft_drinks = (l*k)/nl;
-        int lime_slices = c*d;
-        int salt = p/np;
-        System.out.println(Math.min(salt, Math.min(soft_drinks, lime_slices))/n);
+        String s;
+        while(sc.hasNextLine()){
+            s = sc.nextLine();
+            System.out.println("NO");
+            System.out.flush();
+        }
         sc.close();
     }
 }
