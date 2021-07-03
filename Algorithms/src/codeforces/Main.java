1c0c99e1301b7f721de02e9f1f08ef9d55a2f6a7
Jitendra <jeetscmaker@gmail.com>
2021-07-03 15:30:42 +0530

    contest: Codeforces Round #369 (Div. 2), problem: (A) Bus to Udayland

--- Algorithms/src/codeforces/Main.java
+++ Algorithms/src/codeforces/Main.java
@@ -2,80 +2,45 @@ package codeforces;
 
 import java.util.Scanner;
 
-// contest: Codeforces Round #727 (Div. 2), problem: (B) Love Song, Accepted
+// @author jeetscmaker
+// contest: Codeforces Round #369 (Div. 2), problem: (A) Bus to Udayland
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n, q;
-        n = sc.nextInt();
-        q = sc.nextInt();
-        String s = sc.next();
-        int[] a = new int[n];
+        int n = sc.nextInt();
+        char[][] a = new char[n][5];
         for (int i = 0; i < n; i++) {
-            a[i] = s.charAt(i) - 96;
+            String s = sc.next();
+            int j = 0;
+            for (char c : s.toCharArray()) {
+                a[i][j] = c;
+                j++;
+            }
         }
-        SegmentTree seg = new SegmentTree(0, n - 1, a);
-        long sum = 0;
-        for (int i = 0; i < q; i++) {
-            int l, r;
-            l = sc.nextInt();
-            r = sc.nextInt();
-            sum = seg.rangeSum(l - 1, r - 1);
-            System.out.println(sum);
+        boolean isAvailable = false;
+        OUTER:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 5; j = j + 3) {
+                if (a[i][j] == 'O' && a[i][j + 1] == 'O') {
+                    a[i][j] = '+';
+                    a[i][j + 1] = '+';
+                    isAvailable = true;
+                    break OUTER;
+                }
+
+            }
         }
+        if (isAvailable) {
+            System.out.println("YES\n");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < 5; j++) {
+                    System.out.print(a[i][j]);
+                }
+                System.out.println();
+            }
+
+        } else
+            System.out.println("NO");
         sc.close();
     }
 }
-
-class SegmentTree {
-    int leftMost, rightMost;
-    SegmentTree lChild, rChild;
-    long sum; // for Range Sum Query.
-
-    public SegmentTree(int leftMost, int rightMost, int[] a) {
-        this.leftMost = leftMost;
-        this.rightMost = rightMost;
-        // for leaf
-        if (leftMost == rightMost)
-            sum = a[leftMost];
-        else {
-            int mid = (leftMost + rightMost) / 2;
-            // creating left child and right child
-            // lChild : [leftMost, mid] and rChild : [mid+1, rightMost]
-            lChild = new SegmentTree(leftMost, mid, a);
-            rChild = new SegmentTree(mid + 1, rightMost, a);
-            // re-calculate the range sum.
-            reCalc();
-        }
-    }
-
-    public void reCalc() {
-        if (leftMost == rightMost) return;
-        sum = lChild.sum + rChild.sum;
-    }
-
-    public void pointUpdate(int index, int newValue) {
-        if (leftMost == rightMost) {
-            sum = newValue;
-            return;
-        }
-        // two children
-        if (index < lChild.rightMost) lChild.pointUpdate(index, newValue);
-        else rChild.pointUpdate(index, newValue);
-        // since an update happens, the range sum may also change.  So recalculate it.
-        reCalc();
-    }
-
-    public long rangeSum(int l, int r) {
-        // if the entirely disjoint set comes, that means if the given l is greater than the rightMost index of a
-        // subtree, ignore that subtree. And vice versa.
-        if (l > rightMost || r < leftMost) return 0;
-
-        // If the given [l,r] entirely covers a subtree
-        if (l <= leftMost && r >= rightMost) return sum;
-
-        // else the [l,r] partially covers some subtree.
-        return lChild.rangeSum(l, r) + rChild.rangeSum(l, r);
-    }
-}
-
