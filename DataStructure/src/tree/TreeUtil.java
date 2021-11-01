8bcf3b677c68e56726bd3a2ef984fdd50c4be2f6
Jitendra <jeetscmaker@gmail.com>
2021-11-01 15:16:53 +0530

    TreeUtil.java: a method created for initialising new BinaryTree.

--- DataStructure/src/tree/TreeUtil.java
+++ DataStructure/src/tree/TreeUtil.java
@@ -3,4 +3,22 @@ package tree;
  * A utility class for Tree Data structures.
  * */
 public class TreeUtil {
+
+    public static BinaryTreeNode createBinaryTree(){
+        BinaryTreeNode A = new BinaryTreeNode(1); //                A(1)
+        BinaryTreeNode B = new BinaryTreeNode(2); //              /     \
+        BinaryTreeNode C = new BinaryTreeNode(3); //            B(2)     C(3)
+        BinaryTreeNode D = new BinaryTreeNode(4); //          /     \   /     \
+        BinaryTreeNode E = new BinaryTreeNode(5); //        D(4)  E(5) F(6)   G(7)
+        BinaryTreeNode F = new BinaryTreeNode(6);
+        BinaryTreeNode G = new BinaryTreeNode(7);
+        A.setLeft(B);
+        A.setRight(C);
+        B.setLeft(D);
+        B.setRight(E);
+        C.setLeft(F);
+        C.setRight(G);
+        return A;
+    }
+
 }
