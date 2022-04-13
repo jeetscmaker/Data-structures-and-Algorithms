9bab8c3fab2ef77db04b093e1999d4fb96085ec5
Jitendra Pandey <jeetscmaker@gmail.com>
2022-04-13 22:42:44 +0530

    Minimum Heap in Java
    Min Heap is a special binary tree in which the top element is always the smallest element and the parent element is always smaller than (or equal to) its children. This property is true for all the nodes recursively.

---
+++ DataStructure/src/heap/MinIntHeap.java
@@ -0,0 +1,107 @@
+package test;
+
+import java.util.Arrays;
+
+/**
+ * Min-Heap implementation for integers only.
+ */
+public class MinIntHeap {
+	private int capacity = 10;
+	private int size = 0;
+	int[] items = new int[capacity];
+
+	private int getLeftChildIndex(int index) {
+		return 2 * index + 1;
+	}
+
+	private int getRightChildIndex(int index) {
+		return 2 * index + 2;
+	}
+
+	private int getParentIndex(int childIndex) {
+		return (childIndex - 1) / 2;
+	}
+
+	private boolean hasLeftChild(int index) {
+		return getLeftChildIndex(index) < size;
+	}
+
+	private boolean hasRightChild(int index) {
+		return getRightChildIndex(index) < size;
+	}
+
+	private boolean hasParent(int index) {
+		return getParentIndex(index) >= 0;
+	}
+
+	private int leftChild(int index) {
+		return items[getLeftChildIndex(index)];
+	}
+
+	private int rightChild(int index) {
+		return items[getRightChildIndex(index)];
+	}
+
+	private int parent(int index) {
+		return items[getParentIndex(index)];
+	}
+
+	private void swap(int indexOne, int indexTwo) {
+		int temp = items[indexOne];
+		items[indexOne] = items[indexTwo];
+		items[indexTwo] = temp;
+	}
+
+	private void ensureExtraCapacity() {
+		if (size == capacity) {
+			items = Arrays.copyOf(items, capacity * 2);
+			capacity = capacity * 2;
+		}
+	}
+
+	public int peek() {
+		if (size == 0)
+			throw new IllegalStateException();
+		return items[0];
+	}
+
+	public int poll() {
+		if (size == 0)
+			throw new IllegalStateException();
+		int item = items[0];
+		items[0] = items[size - 1];
+		size--;
+		heapifydown();
+		return item;
+	}
+
+	public void add(int item) {
+		ensureExtraCapacity();
+		items[size] = item;
+		size++;
+		heapifyUp();
+	}
+
+	private void heapifyUp() {
+		int index = size - 1;
+		while (hasParent(index) && parent(index) > items[index]) {
+			swap(getParentIndex(index), index);
+			index = getParentIndex(index);
+		}
+	}
+
+	private void heapifydown() {
+		int index = 0;
+		while (hasLeftChild(index)) {
+			int smallerChildIndex = getLeftChildIndex(index);
+			if (hasRightChild(index) && rightChild(index) < leftChild(index)) {
+				smallerChildIndex = getRightChildIndex(index);
+			}
+			if (items[index] < items[smallerChildIndex])
+				break;
+			else
+				swap(index, smallerChildIndex);
+			index = smallerChildIndex;
+		}
+	}
+}
