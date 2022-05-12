95044b7c98f774a90714e45d1fa741a79b32696d
Jitendra <jeetscmaker@gmail.com>
2022-05-13 00:19:25 +0530

    Code corrected for binary tree data structure

--- DataStructure/src/tree/binary_tree.cpp
+++ DataStructure/src/tree/binary_tree.cpp
@@ -1,37 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
 class binary_tree
 {
 private:
 	int data;
-	binary_tree left;
-	binary_tree right;
+	bool data_assigned = false;
+	binary_tree* left;
+	binary_tree* right;
 public:
-	binary_tree(int data) {
-		this.data = data;
+	binary_tree() {
+		data_assigned = false;
+	}
+	binary_tree(int d) {
+		data = d;
+		data_assigned = true;
+		left = NULL;
+		right = NULL;
 	}
 	~binary_tree(){
-		delete data;
+		data = INT_MIN;
+		data_assigned = false;
 		delete left;
 		delete right;
 	}
-	binary_tree get_left(){
-		return this.left;
+	bool is_data_assigned() {
+		return data_assigned;
 	}
-	binary_tree get_right(){
-		return this.right;
+	int get_data(){
+		return data;
 	}
-	void set_left(binary_tree left){
-		this.left = left;
+	binary_tree* get_left(){
+		return this->left;
 	}
-	void set_right(binary_tree right){
-		this.right = right;
+	binary_tree* get_right(){
+		return this->right;
 	}
-	void print_tree(binary_tree root){
+	void set_data(int d) {
+		data = d;
+	}
+	void set_left(binary_tree* left){
+		this->left = left;
+	}
+	void set_right(binary_tree* right){
+		this->right = right;
+	}
+	void print_tree(binary_tree* root){
 
 	}
-	void preOrder_traverse(binary_tree root);
-	void inorder_traverse(binary_tree root);
-	void postOrder_traverse(binary_tree root);
+	void preOrder_traverse(binary_tree* root);
+	void inorder_traverse(binary_tree* root);
+	void postOrder_traverse(binary_tree* root);
 };
+
+ void binary_tree :: preOrder_traverse(binary_tree* root) {
+	if(root->is_data_assigned()) {
+		// print data, we can process this data as per our need.
+		cout << this->get_data() << " ";
+		preOrder_traverse(root->get_left());
+		preOrder_traverse(root->get_right());
+	}
+}
 
 int main() {
 	binary_tree tree(10);
